b3f56786cb7eeb3ae6777a71f4f61bf094b3b667
Guyutongxue <guyutongxue@163.com>
2025-12-18 01:36:35 +0800

    init commit

---
+++ packages/my-app/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
+
+const config = defineConfig({
+  ssr: {
+    external: true,
+  },
+  plugins: [
+    devtools(),
+    // nitro(),
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+})
+
+export default config
---
+++ packages/my-lib/src/index.cjs
@@ -0,0 +1,3 @@
+exports.getModuleType = function () {
+  return "CJS";
+};
