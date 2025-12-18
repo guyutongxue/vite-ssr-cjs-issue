c7d49e84612ddee28e362aad69689862a2eefea2
Guyutongxue <guyutongxue@163.com>
2025-12-18 12:21:05 +0800

    uniform style

--- packages/ssr-react/src/App.jsx
+++ packages/ssr-react/src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { getModuleType } from 'my-lib'
 
 function App() {
--- packages/ssr-react/src/entry-server.jsx
+++ packages/ssr-react/src/entry-server.jsx
@@ -1,3 +1,4 @@
+import css from './index.css?raw'
 import { StrictMode } from 'react'
 import { renderToString } from 'react-dom/server'
 import App from './App'
@@ -6,10 +7,11 @@ import App from './App'
  * @param {string} _url
  */
 export function render(_url) {
+  const head = `<style>${css}</style>`
   const html = renderToString(
     <StrictMode>
       <App />
     </StrictMode>,
   )
-  return { html }
+  return { head, html }
 }
